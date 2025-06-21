18fe999385275aa2bb2b3ec75d22ee7d69ef7cb0
MiguelA536 <2410307@alumnos.univo.edu.mx>
2025-06-21 17:47:08 -0600

    Primeros avances sobre la lógica de las materias

---
+++ src/main/java/com/mycompany/examen/Materia.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.examen;
+
+/**
+ *
+ * @author migue
+ */
+public class Materia {
+    private String codigo;
+    private String nombre;
+
+    public Materia(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo + ", Nombre: " + nombre;
+    }
+}
